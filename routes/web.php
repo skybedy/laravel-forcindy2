f9976390620725b320291daa2e3ce98fff15c481
skybedy <skybedy@gmail.com>
2024-11-30 14:13:07 +0100

    301124

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [IndexController::class, 'index'])->name('index.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
